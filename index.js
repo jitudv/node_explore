d58a6def9c9d420cb4c74239032a0ae11964fbdb
jitu <jitudv09@gmail.com>
2022-04-12 21:52:44 +0530

    yes its working

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const ExcelJS = require('exceljs')
-console.log("working")
 var fs = require('fs');
 var crypto = require('crypto');
+var excel_handlers  = require('./services/excel_handler')
+var ExcelJS = require('exceljs');
+const { checksum } = require('./services/excel_handler');
+
+test = async (excelFile) =>
+{
+//  var headres  =  await  excel_handlers.getHeaders(excelFile)
+//  console.log('headres ---->>  ',headres)
+//  var checksum =  await excel_handlers.checksum(excelFile)
+//  console.log('checksum   ---->>  ',checksum)
+// var  rowCount = await excel_handlers.getTrailers(excelFile)
+//  console.log('trailers  ---->> ',rowCount)
+}
+
+test('file_example_XLSX_10.xlsx')
+console.log('yes its working  ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 fs.readFile('package.json', function(err, data) {
   var checksum = generateChecksum(data);
@@ -18,12 +57,17 @@ function generateChecksum(str, algorithm, encoding) {
 workbook  = new ExcelJS.Workbook()
 workbook.xlsx.readFile('file_example_XLSX_10.xlsx').then(() => {
   var sheet = workbook.getWorksheet(1);
-  row_headers =[]
   for (var i = 1; i <= sheet.actualRowCount; i++) {
     for (var j = 1; j <= sheet.actualColumnCount; j++) {
-      data = sheet.getRow(i).getCell(j).toString();
+      data = sheet.getRow(i).getCell(j);
       process.stdout.write(data+" ");
     }
-    break;
+   break;
   }
 });
+
+workbook.xlsx.readFile('file_example_XLSX_10.xlsx').then(() => {
+  var sheet = workbook.getWorksheet(1);
+  console.log("number of rows --->> ",sheet.actualRowCount);
+});
+
